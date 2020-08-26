45a42569975b9722e95bda8793ac39588dfb3abd
muhfirmanagebimantara <59757970+muhfirmanagebimantara@users.noreply.github.com>
2020-08-26 15:37:55 +0700

    Template
    selesai

---
+++ application/views/projek/Template/footer.php
@@ -0,0 +1,12 @@
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white" style="text-align: center;">
+        <div class="container my-auto" style="display: inline-block;">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2020</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+   
